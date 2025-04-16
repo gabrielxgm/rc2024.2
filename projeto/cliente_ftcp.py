a787998108e11b82b58246870ce768fd465db33d
gabrielxgm <gabriel.xavier.gomes.maia@ccc.ufcg.edu.br>
2025-04-16 15:06:32 -0300

    Bota comentario

--- projeto/cliente_ftcp.py
+++ projeto/cliente_ftcp.py
@@ -13,7 +13,7 @@ TCP_PORT = int(config['SERVER_CONFIG']['TCP_PORT'])
 def send_request(comando, protocolo, arquivo):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as udp_sock:
-            udp_sock.settimeout(5)
+            udp_sock.settimeout(5) # Defina um tempo de espera para a resposta
             udp_sock.sendto(f"{comando},{protocolo},{arquivo}".encode(), SERVER_ADDRESS)
             print(f"Sent: {comando},{protocolo},{arquivo}")
             data, server = udp_sock.recvfrom(BUFFER_SIZE)
@@ -29,6 +29,16 @@ def send_request(comando, protocolo, arquivo):
     except Exception as e:
         print(f"Error sending request: {e}")
 
+"""
+ Estabelece uma conexão TCP com o servidor e recebe o arquivo solicitado.
+ 
+ Após receber o arquivo, envia uma confirmação (ACK) para o servidor
+ com o número total de bytes recebidos.
+ 
+ Parametros:
+     transfer_port (int): A porta TCP do servidor para a transferência do arquivo.
+     arquivo (str): O nome do arquivo esperado.
+ """
 def receive_file(transfer_port, arquivo):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as tcp_sock:
@@ -44,9 +54,11 @@ def receive_file(transfer_port, arquivo):
                     break
                 received_data += data
 
+            # Exibir o tamanho do arquivo recebido em bytes
             file_size = len(received_data)
             print(f"Received the complete file {arquivo} of size {file_size} bytes")
 
+            # Enviar a confirmação (ACK) com o número de bytes recebidos no formato desejado
             ack_message = f"ftcp_ack,{file_size}".encode()
             tcp_sock.sendall(ack_message)
             print(f"Sent acknowledgment: ftcp_ack,{file_size} bytes")
